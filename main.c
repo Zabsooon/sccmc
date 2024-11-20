5b29e1583b9d5091784ffdd67fe2e9f4634f7a89
Zabsooon <jakub.zabski@protonmail.com>
2024-11-20 17:06:52 +0000

    Add file reading

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char **read_file(char *file_name);
+char *get_source_buffer(FILE *source_file, long long *source_buffer_size);
+long long get_source_size(FILE *source_file);
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr,
+            "Unexpected use of sccmc! (Expected: ./sccmc <sourcefile>)");
+    return 1;
+  }
+
+  FILE *source_file = fopen(argv[1], "r");
+
+  if (source_file == NULL) {
+    fprintf(stderr, "Could not open a file: %s", argv[1]);
+    return 1;
+  }
+
+  long long source_file_size;
+  char *source_file_buffer;
+
+  source_file_buffer = get_source_buffer(source_file, &source_file_size);
+
+  printf("%s", source_file_buffer);
+
+  return 0;
+}
+
+char *
+get_source_buffer(FILE *source_file,
+                  /* return */ long long
+                      *source_buffer_size) { // there should be an easier way
+                                             // for finding the size of source;
+  fpos_t spos;
+  (*source_buffer_size) = get_source_size(source_file);
+
+  char *source_buffer;
+  source_buffer = malloc((*source_buffer_size) * sizeof(char));
+  fseek(source_file, 0, SEEK_SET);
+  fread(source_buffer, sizeof(char), (*source_buffer_size), source_file);
+
+  return source_buffer;
+}
+
+long long get_source_size(FILE *source_file) {
+  fseek(source_file, 0, SEEK_END);
+  long long size = ftell(source_file);
+  fseek(source_file, 0, SEEK_SET);
+  return size;
+}
